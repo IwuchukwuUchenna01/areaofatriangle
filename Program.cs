c1f12825bdf4235ee212cfd9afbebd171b13a9fd
uchenna Iwuchukwu <oreoluwa371@gmail.com>
2023-11-21 21:51:06 +0100

    Made extra modificatiosn to the Documentation

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿internal class Area {
-
-
     public Area()
     {
         double baseLength, height, area;
-
         Console.WriteLine("A simple interactive Program to calculate the area of a triangle\n");
-
         Console.Write("enter the base of the triangle: ");
         baseLength = Double.Parse(Console.ReadLine());
         Console.Write("enter the height of the triangle: ");
@@ -14,12 +10,9 @@
 
         area = height * baseLength / 2;
         Console.WriteLine($"The area of a triangle with baseLength {baseLength} and height {height} is { area}");
-
     }
     public static void Main(String[] args) {
         Area areaData = new Area();
-
-
     }
 
 }
