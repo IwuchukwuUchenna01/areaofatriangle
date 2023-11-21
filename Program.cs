537376edb601edb0163f27b1f27b9555d8e2250c
uchenna Iwuchukwu <oreoluwa371@gmail.com>
2023-11-21 21:23:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Area {
+
+
+    public Area()
+    {
+        double baseLength, height, area;
+
+        Console.WriteLine("A simple interactive Program to calculate the area of a triangle\n");
+
+        Console.Write("enter the base of the triangle: ");
+        baseLength = Double.Parse(Console.ReadLine());
+        Console.Write("enter the height of the triangle: ");
+        height = Double.Parse(Console.ReadLine());
+
+        area = height * baseLength / 2;
+        Console.WriteLine($"The area of a triangle with baseLength {baseLength} and height {height} is { area}");
+
+    }
+    public static void Main(String[] args) {
+        Area areaData = new Area();
+
+
+    }
+
+}
